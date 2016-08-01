a41742b2bc8add44586143da216e2d8cee927ce5
Dave Lively <davelively14@gmail.com>
2016-08-01 16:34:18 -0400

    Added components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react'
+import { render } from 'react-dom'
+
+// import CSS
+import css from './styles/style.styl'
+
+// Import components
+import Main from './components/main.js'
+
+render(<Main />, document.getElementById('root'))
