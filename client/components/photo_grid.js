7abefae6e3984c06930bbb4953d42337732644a2
Dave Lively <davelively14@gmail.com>
2016-08-02 11:44:23 -0400

    Setup picture HTML

--- client/components/photo_grid.js
+++ client/components/photo_grid.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Photo from './photo.js'
 
+// this.props.posts.map((post, i) => <Photo />) creates an instance of our
+// Photo component for every post.
+// Note the ES6 spread operator, which we can read about here:
+// http://redux.js.org/docs/recipes/UsingObjectSpreadOperator.html
+// Note that we needed to include a plugin for Babel in order for this to work.
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        I'm a photo grid
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
