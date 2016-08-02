a8006367af1506b85ce913b8f13aaa9949f0dfab
Dave Lively <davelively14@gmail.com>
2016-08-02 14:36:40 -0400

    Displaying proper single component, started comments

--- client/components/single.js
+++ client/components/single.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Photo from './photo.js'
+import Comments from './comments.js'
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+    const post = this.props.posts[i]
+
     return (
       <div className="single-photo">
-        I'm a Single Grid
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
