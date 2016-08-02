4634b0c8c7affa6e776658e41536f0d085ab958b
Dave Lively <davelively14@gmail.com>
2016-08-02 10:14:58 -0400

    App connects Main to the store

---
+++ client/components/app.js
@@ -0,0 +1,28 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as actionCreators from '../actions/action_creators.js'
+import Main from './main.js'
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch)
+}
+
+// docs:
+// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
+// mapStateToProps: this function will be called any time the state updates
+//   within the store. The state will be passed to the callback function.
+// mapDispatchToProps: Send an object of functions and injects dispatch.
+//
+// The (Main) part is the component with which we're connecting the store. Now,
+// to call a connected Main, we would inject the App component instead. The Main
+// will be rendered, however.
+const App = connect(mapStateToProps, mapDispatchToProps)(Main)
+
+export default App
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -7,7 +7,7 @@ import { render } from 'react-dom'
 import css from './styles/style.styl'
 
 // Import components
-import Main from './components/main.js'
+import App from './components/app.js'
 import Single from './components/single.js'
 import PhotoGrid from './components/photo_grid.js'
 
@@ -22,13 +22,13 @@ import store, { history } from './store.js'
 // React Router variable. Build Router then add Routes. In this one, we're
 // setting the default path "/" to the main component. IndexRoute just indicates
 // a child component that will render with the previous path. Nested paths are
-// what comes later. So /view/23423 will be render Main compoment with a Single
-// component as a child, whereas ("/") will render Main component with a
+// what comes later. So /view/23423 will render an App compoment with a Single
+// component as a child, whereas ("/") will render an App component with a
 // PhotoGrid child.
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
       </Route>
